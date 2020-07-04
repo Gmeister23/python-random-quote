305a5cec64f2cdfdcd56284b77b5ae6829a52ac0
Gmeister23 <gabolyle@outlook.com>
2020-07-03 20:17:41 -0700

    New file with new quotes

--- get-quote.py
+++ get-quote.py
@@ -3,13 +3,17 @@ import random
 def primary():
   # print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
+  f = open("quotes_appended.txt", "w+")
+  #quotes = f.readlines()
+  for i in range(10):
+    f.write("Appended line %d\r" %(i+1))
   f.close()
 
-  last = len(quotes)-1
-  rnd = random.randint(0,last)
-  print(quotes[rnd])
+ # last = len(quotes)-1
+ # rnd = random.randint(0,last)
+ # rnd2 = random.randint(0,last)
+ # print(quotes[rnd] + quotes[rnd2])
+  
 
 if __name__== "__main__":
   primary()
